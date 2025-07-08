54b389643398b7f7dd61ffa4e325a03e10ee40fb
Idemudia-Precious <idemudiapreciousosaze@gmail.com>
2025-07-08 16:05:26 +0100

    chore: implement auth screens

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,7 @@ export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
   platform: "com.presh.foodordering",
+  databaseId: "686d20fb000adb48c2bd",
+  userCollectionId: "686d2128002dd0e2bf09",
+  
 }
