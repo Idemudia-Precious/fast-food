1451998309cfd986828ba9a872714fada078bc7b
Idemudia-Precious <idemudiapreciousosaze@gmail.com>
2025-07-08 16:43:47 +0100

    chore: implement auth functionality

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,5 +1,6 @@
 import CustomButton from "@/components/CustomButton";
 import CustomInput from "@/components/CustomInput";
+import { signIn } from "@/lib/appwrite";
 import { Link, router } from "expo-router";
 import { useState } from "react";
 import { View, Text, Button, Alert } from "react-native";
@@ -20,7 +21,7 @@ const SignIn = () => {
     setIsSubmitting(true);
 
     try {
-      //await signIn({ email, password });
+      await signIn({ email, password });
 
       router.replace("/");
     } catch (error: any) {
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,8 +1,71 @@
+import { CreateUserParams } from "@/type";
+import { Account, Avatars, Client, Databases, ID, Query } from "react-native-appwrite"
+
 export const appwriteConfig = {
-  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
-  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
   platform: "com.presh.foodordering",
   databaseId: "686d20fb000adb48c2bd",
   userCollectionId: "686d2128002dd0e2bf09",
-  
+
+}
+
+export const client = new Client();
+
+client
+  .setEndpoint(appwriteConfig.endpoint)
+  .setProject(appwriteConfig.projectId)
+  .setPlatform(appwriteConfig.platform);
+//.setSelfSigned() // Use this only for development, not recommended for production
+
+export const account = new Account(client);
+export const databases = new Databases(client);
+const avatars = new Avatars(client);
+
+export const createUser = async ({ email, password, name }: CreateUserParams) => {
+  try {
+    const newAccount = await account.create(ID.unique(), email, password, name)
+    if (!newAccount) throw Error;
+
+    await signIn({ email, password });
+
+    const avatarUrl = avatars.getInitialsURL(name);
+
+    return await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      { email, name, accountId: newAccount.$id, avatar: avatarUrl }
+    );
+  } catch (e) {
+    throw new Error(e as string);
+  }
+}
+
+export const signIn = async ({ email, password }: { email: string; password: string }) => {
+  try {
+    return await account.createEmailPasswordSession(email, password);
+  } catch (e) {
+    throw new Error(e as string);
+  }
+}
+
+export const getCurrentUser = async () => {
+    try {
+        const currentAccount = await account.get();
+        if(!currentAccount) throw Error;
+
+        const currentUser = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.userCollectionId,
+            [Query.equal('accountId', currentAccount.$id)]
+        )
+
+        if(!currentUser) throw Error;
+
+        return currentUser.documents[0];
+    } catch (e) {
+        console.log(e);
+        throw new Error(e as string);
+    }
 }
